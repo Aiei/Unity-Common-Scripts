c2318ea6ad05b2974483ec69188a0959f58ba969
Ibnu Athaillah <ibnuaiei@gmail.com>
2014-10-23 05:25:11 +0700

    Lever loader with loading screen

---
+++ Load Screen/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public LoadingScreen loadingScreen;
+
+    public void Load(string levelName)
+    {
+        loadingScreen = (LoadingScreen)Instantiate(loadingScreen);
+        DontDestroyOnLoad(loadingScreen.gameObject);
+        Application.LoadLevel(levelName);
+    }
+}
---
+++ Load Screen/LoadingScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Texture texture;
+
+    protected int levelIndex;
+
+    protected Rect rect;
+
+    public void Start()
+    {
+        levelIndex = Application.loadedLevel;
+        rect = Rect.MinMaxRect(0, 0, Screen.width, Screen.height);
+    }
+
+    public void OnGUI()
+    {
+        if (Application.loadedLevel == levelIndex)
+        {
+            GUI.DrawTexture(rect, texture);
+        }
+    }
+}
